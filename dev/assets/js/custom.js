b3d73ed9095c0b9f6d35b850c8f4573db348500e
Stas <stas_zhornyak@ukr.net>
2023-11-16 18:44:02 +0200

    advantages mobile

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -13,21 +13,6 @@ $(document).ready(function(){
   });
 });
 
-$(document).ready(function(){
-  $('.advantages-row').unslick({
-     responsive: [
-      {
-        settings: slick,
-        breakpoint: 991,
-        settings: {
-          arrows: false,
-          dots: true
-        }
-      },
-    ]
-  });
-});
-
 const burger = document.querySelector('.hamburger');
 burger.addEventListener('click', function(){
     burger.classList.toggle('is-active');
@@ -44,3 +29,31 @@ console.log (checkButton);
 checkButton.forEach (item => {
   item.closest('.input-box').classList.add('checkbox-padding')
 })
+
+mobileOnlySlider("#advantages-slider, true, false, 991");
+
+function mobileOnlySlider ($slidername, $dots, $arrows, $breakpoint){
+  var slider = $($slidername);
+  var settings = {
+    mobileFirst: true,
+    dots: $dots,
+    arrows: $arrows,
+    responsive: [
+      {
+        breakpoint: $breakpoint,
+        settings: "unslick"
+      }
+    ]
+  };
+
+  slider.slick(settings);
+
+  $(window).on("resize", function() {
+    if ($(window).width() > $breakpoint) {
+      return;
+    }
+    if (!slider.hasClass("slick-initialized")) {
+      return slider.slick(settings);
+    }
+  })
+}
